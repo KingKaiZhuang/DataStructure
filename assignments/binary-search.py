8d9ca72b5e28198e5f06c7af0add015e951627f2
KingKaiZhuang <5b0g0024@stust.edu.tw>
2026-04-12 17:18:28 +0800

    binary search 練習

---
+++ assignments/binary-search.py
@@ -0,0 +1,14 @@
+nums = [1, 3, 5, 6]
+left,right=0,len(nums)-1
+search=1
+
+while left<=right:
+    middle=(left+right)//2
+    
+    if nums[middle]<search:
+        left=middle+1
+    elif nums[middle]>search:
+        right=middle-1
+    else:
+        print(middle)
+        break
