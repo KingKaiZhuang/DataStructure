434d01c3a86d7280dbd03205b51f5fda5ca3e398
KingKaiZhuang <5b0g0024@stust.edu.tw>
2026-04-16 18:52:50 +0800

    複習

--- uva/d424. 00105 - The Skyline Problem.py
+++ uva/d424. 00105 - The Skyline Problem.py
@@ -3,35 +3,36 @@ data=[]
 while True:
     try:
         line=input()
-        data.extend(line.split())
+        data.extend(map(int,line.split()))
     except EOFError:
         break
-# define
-max_pos=10005
-min_L=max_pos
-max_R=0
-house=[0]*max_pos
-# outline
+# define every height
+house=[0]*10005
+minN=10005
+maxN=0
 i=0
 while i<len(data):
-    L,H,R=int(data[i]),int(data[i+1]),int(data[i+2])
+    L=data[i]
+    H=data[i+1]
+    R=data[i+2]
     i+=3
-    
-    if L<min_L:
-        min_L=L
-    if R>max_R:
-        max_R=R
+
+    if L<minN:
+        minN=L
+    if R>maxN:
+        maxN=R
 
     for x in range(L,R):
-        if house[x]<H:
+        if H>house[x]:
             house[x]=H
-# 判斷結果
-cur=0
-ans=[]
-for i in range(min_L,max_R+1):
-    if house[i]!=cur:
-        ans.append(str(i))
-        ans.append(str(house[i]))
-        cur=house[i]
+# output
+curH=0
+record=[]
+for i in range(minN,maxN+1):
+    if house[i]!=curH:
+        record.append(str(i))
+        record.append(str(house[i]))
+        curH=house[i]
 
-print(" ".join(ans))
+ans=" ".join(record)
+print(ans)
