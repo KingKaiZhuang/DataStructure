f8709c66277d0a538f7c91f1cb626c0ada86f3e0
KingKaiZhuang <5b0g0024@stust.edu.tw>
2026-04-19 08:49:27 +0800

    複習

--- uva/d087. 00107 - The Cat in the Hat.py
+++ uva/d087. 00107 - The Cat in the Hat.py
@@ -1,38 +1,26 @@
 while True:
-    try:
-        line=input().strip()
-        H,W=map(int,line.split())
-
-        if H==0 and W==0:
-            break
-            
-        if H==1:
-            print("0 1")
-            continue
-
-        N,k=0,0
-        for steps in range(1,40):
-            n1=round(H**(1/steps))
-            n=round(W**(1/steps))
-
-            if n+1==n1 and n1**steps==H and n**steps==W:
-                N=n
-                k=steps
-                break
-        
-        nonWork=0
-        totalHeight=0
-        curCat=1
-        curHeight=H
-
-        for i in range(k+1):
-            if i<k:
-                nonWork+=curCat
-            totalHeight+=curCat*curHeight
-            curCat*=N
-            curHeight//=(N+1)
-        
-        print(f"{nonWork} {totalHeight}")
-                
-    except EOFError:
+    # input
+    H,W=map(int,input().split())
+    if H==0 and W==0:
         break
+    if H==1 and W==0:
+        print("0 1")
+    # (N+1)^k=H, N^k=W
+    N,K=0,0
+    for i in range(1,40):
+        n1=round(H**(1/i))
+        n2=round(W**(1/i))
+
+        if n2+1==n1 and n1**i==H and n2**i==W:
+            N,K=n2,i
+            break
+    # ans
+    nonWorker,totalHeight,curCat,curH=0,0,1,H
+    for j in range(K+1):
+        if j<K:
+            nonWorker+=curCat
+        totalHeight+=curH*curCat
+        curCat*=N
+        curH//=(N+1)
+
+    print(f"{nonWorker} {totalHeight}")
