33c24fd5764b828a46989f6bb0ed7512229be9ac
KingKaiZhuang <5b0g0024@stust.edu.tw>
2026-04-19 13:10:16 +0800

    複習

--- uva/c073. 00101 - The Blocks Problem.py
+++ uva/c073. 00101 - The Blocks Problem.py
@@ -1,49 +1,43 @@
-def search_pos(blocks,n,a):
-    for r in range(n):
-        for c in range(len(blocks[r])):
-            if blocks[r][c]==a:
-                return r,c
+def get_position(blocks,target,n):
+    for i in range(n):
+        for j in range(len(blocks[i])):
+            if blocks[i][j]==target:
+                return i,j
     return -1,-1
 
-def clear_top(blocks,r,c):
-    while len(blocks[r])>c+1:
-        val=blocks[r].pop()
-        blocks[val].append(val)
+def clear_top(blocks,r1,c1):
+    while len(blocks[r1])-1>c1:
+        tmp=blocks[r1].pop()
+        blocks[tmp].append(tmp)
 
 while True:
     try:
+        # input
         n=int(input())
-        blocks=[]
+        blocks=[[i] for i in range(n)]
+        # command
+        while True:
+            line=input()
+            if line=="quit":
+                break
+            command=line.split()
+            act1,n1,act2,n2=command[0],int(command[1]),command[2],int(command[3])
+            # position
+            r1,c1=get_position(blocks,n1,n)
+            r2,c2=get_position(blocks,n2,n)
+            if r1==r2 or n1==n2:
+                continue
+            # clear
+            if act1=="move":
+                clear_top(blocks,r1,c1)
+            if act2=="onto":
+                clear_top(blocks,r2,c2)
+            # move
+            moveList=blocks[r1][c1:]
+            blocks[r2].extend(moveList)
+            blocks[r1]=blocks[r1][:c1]
+        
         for i in range(n):
-            blocks.append([i])
+            print(f"{i}:",*blocks[i])
     except EOFError:
         break
-    
-    while True:
-        line=input()
-        if line=="quit":
-            break
-
-        command=line.split()
-        act1,act2=command[0],command[2]
-        a,b=int(command[1]),int(command[3])
-
-        r1,c1=search_pos(blocks,n,a)
-        r2,c2=search_pos(blocks,n,b)
-
-        if act1=="move":
-            clear_top(blocks,r1,c1)
-        if act2=="onto":
-            clear_top(blocks,r2,c2)
-
-        move_list=blocks[r1][c1:]
-        blocks[r1]=blocks[r1][:c1]
-
-        for u in move_list:
-            blocks[r2].append(u)
-
-    for i in range(n):
-        listNum=""
-        for j in range(len(blocks[i])):
-            listNum+=" "+str(blocks[i][j])
-        print(f"{i}:"+listNum)
