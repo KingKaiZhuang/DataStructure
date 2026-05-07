cd3a101e482c9cb44002c6723de36c5d2d120c09
KingKaiZhuang <5b0g0024@stust.edu.tw>
2026-05-07 18:21:41 +0800

    複習 ugly number

--- uva/d129. 00136 - Ugly Numbers.py
+++ uva/d129. 00136 - Ugly Numbers.py
@@ -1,18 +1,18 @@
-ugly=[1]
 p2,p3,p5=0,0,0
+ugly=[1]
 
 while len(ugly)<1500:
     next2=ugly[p2]*2
     next3=ugly[p3]*3
     next5=ugly[p5]*5
 
-    nextUgly=min(next2,next3,next5)
-    ugly.append(nextUgly)
-    if next2==nextUgly:
-        p2+=1
-    if next3==nextUgly:
-        p3+=1
-    if next5==nextUgly:
-        p5+=1
+    minN=min(next2,next3,next5)
+    ugly.append(minN)
 
+    if next2==minN:
+        p2+=1
+    if next3==minN:
+        p3+=1
+    if next5==minN:
+        p5+=1
 print(f"The 1500'th ugly number is {ugly[-1]}.")
