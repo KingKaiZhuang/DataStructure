0b9573519343ccad140e9d00571448781a161571
KingKaiZhuang <5b0g0024@stust.edu.tw>
2026-06-01 18:01:18 +0800

    複習

--- uva/706 LC-Display.py
+++ uva/706 LC-Display.py
@@ -1,51 +1,55 @@
 while True:
     try:
         line=input().split()
-        if len(line)==0:
-            continue
         s=int(line[0])
         n=line[1]
+
         if s==0 and n=="0":
             break
-        # Define nine numbers
+
+        # Define Seven Json
         digs={
-            '0':[1,1,1,1,1,1,0],
-            '1':[0,1,1,0,0,0,0],
-            '2':[1,1,0,1,1,0,1],
-            '3':[1,1,1,1,0,0,1],
-            '4':[0,1,1,0,0,1,1],
-            '5':[1,0,1,1,0,1,1],
-            '6':[1,0,1,1,1,1,1],
-            '7':[1,1,1,0,0,0,0],
-            '8':[1,1,1,1,1,1,1],
-            '9':[1,1,1,1,0,1,1]
+            "0":[1,1,1,1,1,1,0],
+            "1":[0,1,1,0,0,0,0],
+            "2":[1,1,0,1,1,0,1],
+            "3":[1,1,1,1,0,0,1],
+            "4":[0,1,1,0,0,1,1],
+            "5":[1,0,1,1,0,1,1],
+            "6":[1,0,1,1,1,1,1],
+            "7":[1,1,1,0,0,0,0],
+            "8":[1,1,1,1,1,1,1],
+            "9":[1,1,1,1,0,1,1]
         }
-        # first row -> last row
-        # Define Horizental
-        def horizontal(segIndex):
-            for k,i in enumerate(n):
-                char='-' if digs[i][segIndex]==1 else ' '
-                print(f" {char*s} ",end="")
+        # Def horizantal
+        def horizantal(num):
+            for k,c in enumerate(n):
+                if digs[c][num]==1:
+                    print(f" {'-'*s} ",end="")
+                else:
+                    print(f" {' '*s} ",end="")
                 if k<len(n)-1:
                     print(" ",end="")
             print()
-
-        # Define Verticle
-        def vertical(left,right):
+        # Def verticle
+        def verticle(left,right):
             for _ in range(s):
-                for k,i in enumerate(n):
-                    charL="|" if digs[i][left] else ' '
-                    charR="|" if digs[i][right] else ' '
-                    print(f"{charL}{s*' '}{charR}",end="")
+                for k,c in enumerate(n):
+                    leftDisplay='|' if digs[c][left]==1 else " "
+                    rightDisplay='|' if digs[c][right]==1 else " "
+                    combine=leftDisplay+" "*s+rightDisplay
+                    print(combine,end="")
                     if k<len(n)-1:
                         print(" ",end="")
                 print()
-            
-        horizontal(0)
-        vertical(5,1)
-        horizontal(6)
-        vertical(4,2)
-        horizontal(3)
+                        
+        # Call function
+        horizantal(0)
+        verticle(5,1)
+        horizantal(6)
+        verticle(4,2)
+        horizantal(3)
+        # gap
         print()
+
     except EOFError:
         break
